6d047eb97709a1f617d65aa154cddfa4e0bbd270
imadettamen <imadettamen18@gmail.com>
2023-11-05 19:37:51 +0100

    config service

--- cinema/src/main/java/ma/imet/cinema/CinemaApplication.java
+++ cinema/src/main/java/ma/imet/cinema/CinemaApplication.java
@@ -2,7 +2,9 @@ package ma.imet.cinema;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CinemaApplication {
 
